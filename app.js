461a31daa664613db890d7b25a2a44acd7e6d0ab
RostMarket <edarm65@gmail.com>
2026-01-14 14:54:38 +0300

    reset login logic

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-console.log("RostMarket started");
-
 function login() {
-  const loginInput = document.getElementById("login").value;
-  const passwordInput = document.getElementById("password").value;
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
   const error = document.getElementById("error");
 
-  if (loginInput === "admin" && passwordInput === "admin") {
-    alert("Добро пожаловать, администратор!");
+  if (login === "admin" && password === "1234") {
     window.location.href = "dashboard.html";
   } else {
     error.textContent = "Неверный логин или пароль";
