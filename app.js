39c7a9d2808c44dfdd8067fcc40bfc61804d6f3f
RostMarket <edarm65@gmail.com>
2026-01-14 12:49:01 +0300

    fix login function name

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function login() {
-  const login = document.getElementById("login").value;
-  const password = document.getElementById("password").value;
+function doLogin() {
+  const loginValue = document.getElementById("login").value;
+  const passwordValue = document.getElementById("password").value;
   const error = document.getElementById("error");
 
-  if (login === "admin" && password === "1234") {
+  if (loginValue === "admin" && passwordValue === "1234") {
     alert("Добро пожаловать, администратор!");
   } else {
     error.textContent = "Неверный логин или пароль";
