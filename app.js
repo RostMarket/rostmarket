fe492a17e4db8c9a41e6fd416ed801725d70a584
RostMarket <edarm65@gmail.com>
2026-01-14 12:44:44 +0300

    add app.js login logic

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("RostMarket started");
+function login() {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+  const error = document.getElementById("error");
+
+  if (login === "admin" && password === "1234") {
+    alert("Добро пожаловать, администратор!");
+  } else {
+    error.textContent = "Неверный логин или пароль";
+  }
+}
