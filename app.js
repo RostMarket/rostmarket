9820353a2e2074647bf1a654b1af335328c1e2a8
RostMarket <edarm65@gmail.com>
2026-01-14 15:21:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 function login() {
   const login = document.getElementById("login").value;
   const password = document.getElementById("password").value;
-  const error = document.getElementById("error");
 
   if (login === "admin" && password === "1234") {
+    localStorage.setItem("auth", "true");
     window.location.href = "dashboard.html";
   } else {
-    error.textContent = "Неверный логин или пароль";
+    document.getElementById("error").innerText = "Неверный логин или пароль";
+  }
+}
+
+function logout() {
+  localStorage.removeItem("auth");
+  window.location.href = "index.html";
+}
+
+function goProducts() {
+  window.location.href = "products.html";
+}
+
+function back() {
+  window.location.href = "dashboard.html";
+}
+
+if (
+  location.pathname.includes("dashboard") ||
+  location.pathname.includes("products")
+) {
+  if (localStorage.getItem("auth") !== "true") {
+    window.location.href = "index.html";
   }
 }
